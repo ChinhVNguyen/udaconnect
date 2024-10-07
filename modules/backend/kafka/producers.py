c3709f463cef4b655e19b12b199005bc1946b7f8
ChinhVNguyen <chinhfptusa@gmail.com>
2024-10-07 20:17:59 +0700

    Update consumers topic

--- modules/backend/kafka/producers.py
+++ modules/backend/kafka/producers.py
@@ -4,5 +4,5 @@ kafka_server = os.environ["KAFKA_API"]
 
 producer = KafkaProducer(bootstrap_servers=kafka_server)
 msg1=dict({'first_name':'Ket','last_name':'Nguyen','company_name':'FPT'})
-producer.send('person', bytes(str(msg1), 'utf-8'))
+producer.send('persons', bytes(str(msg1), 'utf-8'))
 producer.flush()
