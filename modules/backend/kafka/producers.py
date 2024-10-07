a5d4b44906b66e9ddfaf666a62ffae4f1dc2e8c5
ChinhVNguyen <chinhfptusa@gmail.com>
2024-10-07 19:54:24 +0700

    Update kafka server

--- modules/backend/kafka/producers.py
+++ modules/backend/kafka/producers.py
@@ -1,5 +1,9 @@
+import os
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+kafka_server = os.environ["KAFKA_API"]
+kafka_topic = os.environ["KAFKA_TOPIC"]
+
+producer = KafkaProducer(bootstrap_servers=kafka_server)
 msg1=dict({'first_name':'Ket','last_name':'Nguyen','company_name':'FPT'})
 producer.send('person', bytes(str(msg1), 'utf-8'))
 producer.flush()
